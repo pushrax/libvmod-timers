4c8ec18502d9724f4830e53937181bc36f02f275
Justin Li <jli.justinli@gmail.com>
2014-07-26 17:11:39 -0400

    Add duration2int function

--- src/vmod_timers.c
+++ src/vmod_timers.c
@@ -3,6 +3,10 @@
 #include "vcc_if.h"
 
 // Returns the end of request reading, which is the start of request processing.
-VCL_TIME vmod_req_start(const struct vrt_ctx *ctx, struct vmod_priv *priv) {
+VCL_TIME vmod_req_start(const struct vrt_ctx *ctx) {
 	return ctx->req->t_req;
+}
+
+VCL_INT vmod_duration2int(const struct vrt_ctx *ctx, VCL_DURATION dur, VCL_INT mult) {
+	return dur * mult;
 }

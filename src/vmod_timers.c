1b8530b9f61fe31a72cdc42de8664d2d60cd7fe7
Justin Li <jli.justinli@gmail.com>
2014-07-26 16:51:25 -0400

    Clarify what req_start is

--- src/vmod_timers.c
+++ src/vmod_timers.c
@@ -2,6 +2,7 @@
 #include "cache/cache.h"
 #include "vcc_if.h"
 
+// Returns the end of request reading, which is the start of request processing.
 VCL_REAL vmod_req_start(const struct vrt_ctx *ctx, struct vmod_priv *priv) {
 	return ctx->req->t_req;
 }

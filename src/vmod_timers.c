ea52e56bad07797474aa83317f6a70f0f0110540
Justin Li <jli.justinli@gmail.com>
2014-07-26 16:30:34 -0400

    Delete optional init function

--- src/vmod_timers.c
+++ src/vmod_timers.c
@@ -2,10 +2,6 @@
 #include "cache/cache.h"
 #include "vcc_if.h"
 
-int init_function(struct vmod_priv *priv, const struct VCL_conf *conf) {
-	return (0);
-}
-
 VCL_REAL vmod_req_start(const struct vrt_ctx *ctx, struct vmod_priv *priv) {
 	return ctx->req->t_req;
 }

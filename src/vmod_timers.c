e99ba482f901a17221597bbfff97a6aa2bf265cf
Justin Li <jli.justinli@gmail.com>
2014-07-26 09:38:11 -0400

    Return a VCL_TIME instead

--- src/vmod_timers.c
+++ src/vmod_timers.c
@@ -3,6 +3,6 @@
 #include "vcc_if.h"
 
 // Returns the end of request reading, which is the start of request processing.
-VCL_REAL vmod_req_start(const struct vrt_ctx *ctx, struct vmod_priv *priv) {
+VCL_TIME vmod_req_start(const struct vrt_ctx *ctx, struct vmod_priv *priv) {
 	return ctx->req->t_req;
 }
